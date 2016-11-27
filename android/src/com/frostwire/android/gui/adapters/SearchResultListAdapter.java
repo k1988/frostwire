b88b9a623334fdc288d23361e3e70bb4309d6e4e
gubatron <gubatron@gmail.com>
2016-11-27 12:57:28 -0700

    [android] make sure data set is invalidated when applying filters

--- android/src/com/frostwire/android/gui/adapters/SearchResultListAdapter.java
+++ android/src/com/frostwire/android/gui/adapters/SearchResultListAdapter.java
@@ -269,14 +269,22 @@ public abstract class SearchResultListAdapter extends AbstractListAdapter<Search
     public void setKeywordFiltersPipeline(List<KeywordFilter> keywordFiltersPipeline) {
         this.keywordFiltersPipeline.clear();
         this.keywordFiltersPipeline.addAll(keywordFiltersPipeline);
+        notifyDataSetInvalidated();
     }
 
     public void addKeywordFilter(KeywordFilter kf) {
         this.keywordFiltersPipeline.add(kf);
+        notifyDataSetInvalidated();
     }
 
     public void removeKeywordFilter(KeywordFilter kf) {
         this.keywordFiltersPipeline.remove(kf);
+        notifyDataSetInvalidated();
+    }
+
+    public void clearKeywordFilters() {
+        this.keywordFiltersPipeline.clear();
+        notifyDataSetInvalidated();
     }
 
     private static class OnLinkClickListener implements OnClickListener {
